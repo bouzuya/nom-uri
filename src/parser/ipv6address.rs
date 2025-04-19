ca6be848fbbb1683f6570e65dfd9af80326df5c5
bouzuya <m@bouzuya.net>
2025-04-19 09:10:02 +0900

    Add ipv6address parser

--- src/parser/ipv6address.rs
+++ src/parser/ipv6address.rs
@@ -14,11 +14,345 @@ impl<'a> HasSpan<'a> for Token<'a> {
     }
 }
 
+/// IPv6address =                            6( h16 ":" ) ls32
+///             /                       "::" 5( h16 ":" ) ls32
+///             / [               h16 ] "::" 4( h16 ":" ) ls32
+///             / [ *1( h16 ":" ) h16 ] "::" 3( h16 ":" ) ls32
+///             / [ *2( h16 ":" ) h16 ] "::" 2( h16 ":" ) ls32
+///             / [ *3( h16 ":" ) h16 ] "::"    h16 ":"   ls32
+///             / [ *4( h16 ":" ) h16 ] "::"              ls32
+///             / [ *5( h16 ":" ) h16 ] "::"              h16
+///             / [ *6( h16 ":" ) h16 ] "::"
+///
+/// <https://datatracker.ietf.org/doc/html/rfc3986#section-3.2.2>
+pub fn ipv6address(i: Span) -> IResult<Span, Token> {
+    fn f<'a, F>(i: Span<'a>, mut parser: F, tag: &'static str) -> IResult<Span<'a>, Span<'a>>
+    where
+        F: nom::Parser<Span<'a>, Error = nom::error::Error<Span<'a>>>,
+    {
+        use nom::error::ParseError;
+
+        let start = i.clone();
+        let (i, i2) = nom::bytes::take_until(tag).parse(i)?;
+        let (i3, _) = parser.parse(i2)?;
+        if i3.input_len() != 0 {
+            return Err(nom::Err::Error(
+                <F as Parser<Span<'a>>>::Error::from_error_kind(
+                    start,
+                    nom::error::ErrorKind::ManyMN,
+                ),
+            ));
+        }
+        Ok((i.clone(), start.take(start.offset(&i))))
+    }
+
+    let start = i;
+    let (i, _) = nom::branch::alt((
+        // =                            6( h16 ":" ) ls32
+        (
+            nom::multi::count((h16, nom::character::complete::char(':')), 6),
+            ls32,
+        )
+            .map(|_| ()),
+        // /                       "::" 5( h16 ":" ) ls32
+        (
+            nom::bytes::complete::tag("::"),
+            nom::multi::count((h16, nom::character::complete::char(':')), 5),
+            ls32,
+        )
+            .map(|_| ()),
+        // / [               h16 ] "::" 4( h16 ":" ) ls32
+        (
+            |i| f(i, nom::combinator::opt(h16), "::"),
+            // nom::combinator::opt(h16),
+            nom::bytes::complete::tag("::"),
+            nom::multi::count((h16, nom::character::complete::char(':')), 4),
+            ls32,
+        )
+            .map(|_| ()),
+        // / [ *1( h16 ":" ) h16 ] "::" 3( h16 ":" ) ls32
+        (
+            nom::bytes::complete::tag("::"),
+            nom::multi::count((h16, nom::character::complete::char(':')), 3),
+            ls32,
+        )
+            .map(|_| ()),
+        (
+            |i| {
+                f(
+                    i,
+                    (
+                        nom::multi::many_m_n(0, 1, (h16, nom::character::complete::char(':'))),
+                        h16,
+                    ),
+                    "::",
+                )
+            },
+            nom::bytes::complete::tag("::"),
+            nom::multi::count((h16, nom::character::complete::char(':')), 3),
+            ls32,
+        )
+            .map(|_| ()),
+        // / [ *2( h16 ":" ) h16 ] "::" 2( h16 ":" ) ls32
+        (
+            nom::bytes::complete::tag("::"),
+            nom::multi::count((h16, nom::character::complete::char(':')), 2),
+            ls32,
+        )
+            .map(|_| ()),
+        (
+            // nom::multi::many_m_n(0, 2, (h16, nom::character::complete::char(':'))),
+            // nom::multi::count((h16, nom::character::complete::char(':')), 2),
+            // h16,
+            |i| {
+                f(
+                    i,
+                    (
+                        nom::multi::many_m_n(0, 2, (h16, nom::character::complete::char(':'))),
+                        h16,
+                    ),
+                    "::",
+                )
+            },
+            nom::bytes::complete::tag("::"),
+            nom::multi::count((h16, nom::character::complete::char(':')), 2),
+            ls32,
+        )
+            .map(|_| ()),
+        // / [ *3( h16 ":" ) h16 ] "::"    h16 ":"   ls32
+        (
+            nom::bytes::complete::tag("::"),
+            (h16, nom::character::complete::char(':')),
+            ls32,
+        )
+            .map(|_| ()),
+        (
+            |i| {
+                f(
+                    i,
+                    (
+                        nom::multi::many_m_n(0, 3, (h16, nom::character::complete::char(':'))),
+                        h16,
+                    ),
+                    "::",
+                )
+            },
+            nom::bytes::complete::tag("::"),
+            (h16, nom::character::complete::char(':')),
+            ls32,
+        )
+            .map(|_| ()),
+        // / [ *4( h16 ":" ) h16 ] "::"              ls32
+        (nom::bytes::complete::tag("::"), ls32).map(|_| ()),
+        (
+            |i| {
+                f(
+                    i,
+                    (
+                        nom::multi::many_m_n(0, 4, (h16, nom::character::complete::char(':'))),
+                        h16,
+                    ),
+                    "::",
+                )
+            },
+            nom::bytes::complete::tag("::"),
+            ls32,
+        )
+            .map(|_| ()),
+        // / [ *5( h16 ":" ) h16 ] "::"              h16
+        (nom::bytes::complete::tag("::"), h16).map(|_| ()),
+        (
+            |i| {
+                f(
+                    i,
+                    (
+                        nom::multi::many_m_n(0, 5, (h16, nom::character::complete::char(':'))),
+                        h16,
+                    ),
+                    "::",
+                )
+            },
+            nom::bytes::complete::tag("::"),
+            h16,
+        )
+            .map(|_| ()),
+        // / [ *6( h16 ":" ) h16 ] "::"
+        nom::bytes::complete::tag("::").map(|_| ()),
+        (
+            |i| {
+                f(
+                    i,
+                    (
+                        nom::multi::many_m_n(0, 6, (h16, nom::character::complete::char(':'))),
+                        h16,
+                    ),
+                    "::",
+                )
+            },
+            nom::bytes::complete::tag("::"),
+        )
+            .map(|_| ()),
+    ))
+    .parse(i)?;
+    Ok((
+        i,
+        Token {
+            span: start.take(start.offset(&i)),
+        },
+    ))
+}
+
 #[cfg(test)]
 mod tests {
     use crate::parser::tests::{err, ok};
 
     use super::*;
+
+    #[test]
+    fn test_ipv6address() {
+        // 6( h16 ":" ) ls32
+        ok(
+            ipv6address,
+            "1111:2222:3333:4444:5555:6666:7777:8888",
+            ("", "1111:2222:3333:4444:5555:6666:7777:8888"),
+        );
+        ok(
+            ipv6address,
+            "1111:2222:3333:4444:5555:6666:127.0.0.1",
+            ("", "1111:2222:3333:4444:5555:6666:127.0.0.1"),
+        );
+
+        // "::" 5( h16 ":" ) ls32
+        ok(
+            ipv6address,
+            "::1111:2222:3333:4444:5555:6666:7777",
+            ("", "::1111:2222:3333:4444:5555:6666:7777"),
+        );
+        ok(
+            ipv6address,
+            "::1111:2222:3333:4444:5555:192.168.0.1",
+            ("", "::1111:2222:3333:4444:5555:192.168.0.1"),
+        );
+
+        // [ h16 ] "::" 4( h16 ":" ) ls32
+        ok(
+            ipv6address,
+            "1111::2222:3333:4444:5555:6666:7777",
+            ("", "1111::2222:3333:4444:5555:6666:7777"),
+        );
+        ok(
+            ipv6address,
+            "1111::2222:3333:4444:5555:127.0.0.1",
+            ("", "1111::2222:3333:4444:5555:127.0.0.1"),
+        );
+        ok(
+            ipv6address,
+            "::2222:3333:4444:5555:6666:7777",
+            ("", "::2222:3333:4444:5555:6666:7777"),
+        );
+
+        // [ *1( h16 ":" ) h16 ] "::" 3( h16 ":" ) ls32
+        ok(
+            ipv6address,
+            "1111:2222::3333:4444:5555:6666:7777",
+            ("", "1111:2222::3333:4444:5555:6666:7777"),
+        );
+        ok(
+            ipv6address,
+            "1111:2222::3333:4444:5555:127.0.0.1",
+            ("", "1111:2222::3333:4444:5555:127.0.0.1"),
+        );
+        ok(
+            ipv6address,
+            "::3333:4444:5555:6666:7777",
+            ("", "::3333:4444:5555:6666:7777"),
+        );
+
+        // [ *2( h16 ":" ) h16 ] "::" 2( h16 ":" ) ls32
+        ok(
+            ipv6address,
+            "1111:2222:3333::4444:5555:6666:7777",
+            ("", "1111:2222:3333::4444:5555:6666:7777"),
+        );
+        ok(
+            ipv6address,
+            "1111:2222:3333::4444:5555:127.0.0.1",
+            ("", "1111:2222:3333::4444:5555:127.0.0.1"),
+        );
+        ok(
+            ipv6address,
+            "::4444:5555:6666:7777",
+            ("", "::4444:5555:6666:7777"),
+        );
+
+        // [ *3( h16 ":" ) h16 ] "::" h16 ":" ls32
+        ok(
+            ipv6address,
+            "1111:2222:3333:4444::5555:6666:7777",
+            ("", "1111:2222:3333:4444::5555:6666:7777"),
+        );
+        ok(
+            ipv6address,
+            "1111:2222:3333:4444::5555:127.0.0.1",
+            ("", "1111:2222:3333:4444::5555:127.0.0.1"),
+        );
+        ok(ipv6address, "::5555:6666:7777", ("", "::5555:6666:7777"));
+
+        // [ *4( h16 ":" ) h16 ] "::" ls32
+        ok(
+            ipv6address,
+            "1111:2222:3333:4444:5555::6666:7777",
+            ("", "1111:2222:3333:4444:5555::6666:7777"),
+        );
+        ok(
+            ipv6address,
+            "1111:2222:3333:4444:5555::127.0.0.1",
+            ("", "1111:2222:3333:4444:5555::127.0.0.1"),
+        );
+        ok(ipv6address, "::6666:7777", ("", "::6666:7777"));
+        ok(ipv6address, "::127.0.0.1", ("", "::127.0.0.1"));
+
+        // [ *5( h16 ":" ) h16 ] "::" h16
+        ok(
+            ipv6address,
+            "1111:2222:3333:4444:5555:6666::7777",
+            ("", "1111:2222:3333:4444:5555:6666::7777"),
+        );
+        ok(ipv6address, "::7777", ("", "::7777"));
+
+        // [ *6( h16 ":" ) h16 ] "::"
+        ok(
+            ipv6address,
+            "1111:2222:3333:4444:5555:6666:7777::",
+            ("", "1111:2222:3333:4444:5555:6666:7777::"),
+        );
+        ok(ipv6address, "::", ("", "::"));
+
+        // Test case sensitivity
+        ok(
+            ipv6address,
+            "abcd:EF01:2345:6789:abcd:EF01:2345:6789",
+            ("", "abcd:EF01:2345:6789:abcd:EF01:2345:6789"),
+        );
+
+        // Test with trailing characters
+        // ok(ipv6address, "::7777/64", ("/64", "::7777"));
+        ok(
+            ipv6address,
+            // "2001:db8::1",
+            "2001:db8::1:0:0:1/64",
+            // "2001:db8::1:/",
+            // ("", ""),
+            ("/64", "2001:db8::1:0:0:1"),
+        );
+
+        ok(ipv6address, "2001:db8:::1", (":1", "2001:db8::"));
+        ok(ipv6address, "2001:db8::1::", ("::", "2001:db8::1"));
+        ok(ipv6address, ":::1", (":1", "::"));
+        ok(ipv6address, "1:::", (":", "1::"));
+        ok(ipv6address, "1:2:3:4:5:6:7:8:9", (":9", "1:2:3:4:5:6:7:8"));
+        err(ipv6address, "1:2:3");
+    }
 
     #[test]
     fn test_lookahead() {
@@ -177,5 +511,36 @@ mod tests {
         ok(h3, "1:2:3::", ("", "1:2:3::"));
         ok(h3, "1:2::", ("", "1:2::"));
         ok(h3, "1::", ("", "1::"));
+
+        fn h4_<'a, F>(i: Span<'a>, parser: F, t: &'static str) -> IResult<Span<'a>, Token<'a>>
+        where
+            F: nom::Parser<Span<'a>, Error = nom::error::Error<Span<'a>>>,
+        {
+            let start = i;
+            let (i, _) = Lookahead::new(parser, t).parse(i)?;
+            Ok((
+                i,
+                Token {
+                    span: start.take(start.offset(&i)),
+                },
+            ))
+        }
+
+        fn h4(i: Span) -> IResult<Span, Token> {
+            h4_(
+                i,
+                (
+                    nom::multi::many_m_n(0, 2, (h16, nom::character::complete::char(':'))),
+                    h16,
+                ),
+                "::",
+            )
+        }
+
+        err(h4, "1:2:3:4:5::");
+        err(h4, "1:2:3:4::");
+        ok(h4, "1:2:3::", ("", "1:2:3::"));
+        ok(h4, "1:2::", ("", "1:2::"));
+        ok(h4, "1::", ("", "1::"));
     }
 }
